8f460550dcaaf09563e72e0b4c5e2ba72cf83531
strobie <0xstrobe@protonmail.com>
2023-11-25 17:40:23 +0100

    ije

---
+++ daemon.ts
@@ -0,0 +1,44 @@
+import { CpuInfo, cpus } from "os";
+import { createClient } from "redis";
+import { config } from "dotenv";
+config();
+
+const client = createClient({ url: process.env.REDIS_URL });
+client.on("error", (error) => {
+  console.error(error);
+});
+const key = "cpu_usage";
+const setCpuUsage = async (cpusUsage: number[]) => {
+  const payload = {
+    cpusUsage,
+    timestamp: Date.now(),
+  };
+  await client.set(key, JSON.stringify(payload));
+};
+
+function calculateCpuUsage(oldCpus: CpuInfo[], newCpus: CpuInfo[]) {
+  const cpusUsage: number[] = [];
+  for (let i = 0; i < newCpus.length; i++) {
+    const oldCpu = oldCpus[i];
+    const newCpu = newCpus[i];
+    const totalDiff = Object.keys(newCpu.times).reduce((total, key) => {
+      return total + (newCpu.times[key] - oldCpu.times[key]);
+    }, 0);
+
+    const idleDiff = newCpu.times.idle - oldCpu.times.idle;
+    cpusUsage.push((totalDiff - idleDiff) / totalDiff);
+  }
+  return cpusUsage;
+}
+
+function monitorCpuUsage(interval = 1000) {
+  let oldCpus = cpus();
+  setInterval(() => {
+    const newCpus = cpus();
+    const cpusUsage = calculateCpuUsage(oldCpus, newCpus);
+    setCpuUsage(cpusUsage);
+    oldCpus = newCpus;
+  }, interval);
+}
+
+monitorCpuUsage();
